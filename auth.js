e8eebf436e0610016138f6ece7630517b9cd3da2
StaticWeber <buntulu23@gmail.com>
2025-09-13 23:24:15 -0700

    first commit

---
+++ auth.js
@@ -0,0 +1,43 @@
+const authenticateForm = document.getElementById('authenticate-form');
+const maleUser = document.getElementById('male');
+
+
+function check(){
+    if(localStorage.getItem('credentials')){
+        setTimeout(() => {
+          window.location.href = 'home.html'
+        }, 1000);
+        
+    }
+}
+
+check();
+
+
+function Authenticate(e){
+    let gender;
+    e.preventDefault();
+
+    maleUser.checked ?
+        gender = {
+            male: "male"
+        } :
+        
+        gender = {
+            female: "female"
+        }
+    
+
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const country = document.getElementById('country').value;
+    // !name || !age || !country ? console.log('no data') :
+    localStorage.setItem('credentials', JSON.stringify({ name, age, country, gender }));
+    window.alert('Authentication success!');
+    setTimeout(() => {
+        window.location.href = 'home.html';
+    }, 3000);
+
+}
+
+authenticateForm.addEventListener('submit', Authenticate);
---
+++ home.js
@@ -0,0 +1,18 @@
+const Usercreditials = JSON.parse(localStorage.getItem('credentials'));
+const special = document.getElementsByClassName('special');
+
+function VerifyAuth(){
+if(!Usercreditials){
+    window.alert('Please Authenticate first');
+
+    setTimeout(() => {
+       window.location.href = 'auth.html'
+    }, 1000)
+   
+} else{
+    special[0].textContent = Usercreditials.name;
+}
+
+}
+
+VerifyAuth();
